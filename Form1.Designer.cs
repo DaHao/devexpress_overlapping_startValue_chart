6ebe5b016aff0c73752c81fba81f21a80d874410
DaHao <jiash561@gmail.com>
2016-12-16 13:54:02 +0800

    whole program

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace devexpress_overlapping_startValue_chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.XYDiagramPane xyDiagramPane1 = new DevExpress.XtraCharts.XYDiagramPane();
+            DevExpress.XtraCharts.SecondaryAxisY secondaryAxisY1 = new DevExpress.XtraCharts.SecondaryAxisY();
+            DevExpress.XtraCharts.SecondaryAxisY secondaryAxisY2 = new DevExpress.XtraCharts.SecondaryAxisY();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView1 = new DevExpress.XtraCharts.LineSeriesView();
+            DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView2 = new DevExpress.XtraCharts.LineSeriesView();
+            DevExpress.XtraCharts.Series series3 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView3 = new DevExpress.XtraCharts.LineSeriesView();
+            DevExpress.XtraCharts.Series series4 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView4 = new DevExpress.XtraCharts.LineSeriesView();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagramPane1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(secondaryAxisY1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(secondaryAxisY2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1;0";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            xyDiagram1.PaneLayoutDirection = DevExpress.XtraCharts.PaneLayoutDirection.Horizontal;
+            xyDiagramPane1.Name = "Pane 1";
+            xyDiagramPane1.PaneID = 0;
+            xyDiagram1.Panes.AddRange(new DevExpress.XtraCharts.XYDiagramPane[] {
+            xyDiagramPane1});
+            secondaryAxisY1.AxisID = 0;
+            secondaryAxisY1.Name = "Secondary AxisY 1";
+            secondaryAxisY1.VisibleInPanesSerializable = "-1";
+            secondaryAxisY2.AxisID = 1;
+            secondaryAxisY2.GridLines.Visible = true;
+            secondaryAxisY2.Name = "Secondary AxisY 2";
+            secondaryAxisY2.VisibleInPanesSerializable = "0";
+            xyDiagram1.SecondaryAxesY.AddRange(new DevExpress.XtraCharts.SecondaryAxisY[] {
+            secondaryAxisY1,
+            secondaryAxisY2});
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Center;
+            this.chartControl1.Legend.AlignmentVertical = DevExpress.XtraCharts.LegendAlignmentVertical.TopOutside;
+            this.chartControl1.Legend.Direction = DevExpress.XtraCharts.LegendDirection.LeftToRight;
+            this.chartControl1.Legend.UseCheckBoxes = true;
+            this.chartControl1.Location = new System.Drawing.Point(0, 0);
+            this.chartControl1.Name = "chartControl1";
+            series1.Name = "Series 1";
+            series1.View = lineSeriesView1;
+            series2.Name = "Series 2";
+            lineSeriesView2.AxisYName = "Secondary AxisY 1";
+            series2.View = lineSeriesView2;
+            series3.Name = "Series 3";
+            lineSeriesView3.AxisYName = "Secondary AxisY 2";
+            lineSeriesView3.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(112)))), ((int)(((byte)(192)))));
+            lineSeriesView3.PaneName = "Pane 1";
+            series3.View = lineSeriesView3;
+            series4.Name = "Series 4";
+            lineSeriesView4.AxisYName = "Secondary AxisY 2";
+            lineSeriesView4.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(80)))), ((int)(((byte)(77)))));
+            lineSeriesView4.PaneName = "Pane 1";
+            series4.View = lineSeriesView4;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1,
+        series2,
+        series3,
+        series4};
+            this.chartControl1.Size = new System.Drawing.Size(987, 406);
+            this.chartControl1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(112, 27);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(70, 14);
+            this.labelControl2.TabIndex = 5;
+            this.labelControl2.Text = "labelControl2";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(112, 7);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(70, 14);
+            this.labelControl1.TabIndex = 4;
+            this.labelControl1.Text = "labelControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 406);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chartControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(xyDiagramPane1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(secondaryAxisY1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(secondaryAxisY2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private System.Windows.Forms.Button button1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+
+
+namespace devexpress_overlapping_startValue_chart
+{
+    public partial class Form1 : Form
+    {
+        Series s { get { return chartControl1.Series["Series 1"]; } }
+        Series ss { get { return chartControl1.Series["Series 2"]; } }
+        Series sss { get { return chartControl1.Series["Series 3"]; } }
+        Series ssss { get { return chartControl1.Series["Series 4"]; } }
+
+        AxisRange s1_AxisRange = null, s2_AxisRange = null;
+
+        List<DateTime> dateList = new List<DateTime>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Series s in chartControl1.Series)
+                s.Points.Clear();
+
+            Do();
+        }
+
+        /// <summary>
+        /// 取得DrawAmpChart資料
+        /// </summary>
+        private void Do()
+        {
+            dateList = GetDate();
+
+            #region "塞資料"
+            Random rn = new Random();
+
+            List<SeriesPoint> SPs = new List<SeriesPoint>();
+            List<SeriesPoint> SPs2 = new List<SeriesPoint>();
+
+            //開始的價錢(參考價)
+            double s1_startValue = rn.Next(1, 10);
+            double s2_startValue = rn.Next(1, 10);
+
+            //亂數填值
+            foreach (DateTime dt in dateList)
+            {
+                double v1, v2, v1_RF, v2_RF;
+                do
+                {
+                    v1 = s1_startValue + Math.Round((rn.NextDouble() - 0.5), 2);
+                    v2 = s2_startValue + Math.Round((rn.NextDouble() - 0.5), 2);
+
+                    v1_RF = Math.Abs(v1 - s1_startValue) / s1_startValue;
+                    v2_RF = Math.Abs(v2 - s2_startValue) / s2_startValue;
+                }
+                while (!(v1_RF <= 0.1 && v2_RF <= 0.1));
+
+                SeriesPoint sp = new SeriesPoint();
+                sp.DateTimeArgument = dt;
+                sp.Values = new double[] { v1 };
+                SPs.Add(sp);
+
+                SeriesPoint sp2 = new SeriesPoint();
+                sp2.DateTimeArgument = dt;
+                sp2.Values = new double[] { v2 };
+                SPs2.Add(sp2);
+            }
+
+            s.Points.AddRange(SPs.ToArray());
+            ss.Points.AddRange(SPs2.ToArray());
+            #endregion
+
+            #region "設定AxisRange"
+            s1_startValue = (SPs.First() as SeriesPoint).Values[0];
+            s2_startValue = (SPs2.First() as SeriesPoint).Values[0];
+
+            double s1_HighValue = SPs.Max(x => x.Values[0]);
+            double s2_HighValue = SPs2.Max(x => x.Values[0]);
+
+            double s1_LowValue = SPs.Min(x => x.Values[0]);
+            double s2_LowValue = SPs2.Min(x => x.Values[0]);
+
+            s1_AxisRange = new AxisRange(s1_startValue, s1_HighValue, s1_LowValue);
+            s2_AxisRange = new AxisRange(s2_startValue, s2_HighValue, s2_LowValue);
+            #endregion
+
+            //計算s1、s2的漲跌幅填入s2、s4，s1跟s2的起伏應該要與s3、s4一致
+            CaluAmp(SPs, SPs2);
+
+            AdjustAxis();
+        }
+
+        /// <summary>
+        /// 調整AxisValue
+        /// </summary>
+        private void AdjustAxis()
+        {
+            ////比例調整，取最高及最低的倍率
+            double percentH = Math.Max(s1_AxisRange.HighProportion, s2_AxisRange.HighProportion);
+            double percentL = Math.Min(s1_AxisRange.LowProportion, s2_AxisRange.LowProportion);
+
+            // 各乘上一相同比例的倍率，使原點會重合
+            double s1_axisH = s1_AxisRange.GetAxisValue(percentH);
+            double s1_axisL = s1_AxisRange.GetAxisValue(percentL);
+
+            double s2_axisH = s2_AxisRange.GetAxisValue(percentH);
+            double s2_axisL = s2_AxisRange.GetAxisValue(percentL);
+
+            (chartControl1.Diagram as XYDiagram).AxisY.WholeRange.SetMinMaxValues(s1_axisL, s1_axisH);
+            (chartControl1.Diagram as XYDiagram).SecondaryAxesY[0].WholeRange.SetMinMaxValues(s2_axisL, s2_axisH);
+
+
+            labelControl1.Text = "倍率：(上)" + percentH + ", (下)" + percentL;
+            labelControl2.Text = "s1:   " + s1_AxisRange.HighValue + "   " + s1_AxisRange.LowValue + "   s2:   " + s2_AxisRange.HighValue + ", " + s2_AxisRange.LowValue;
+        }
+
+        /// <summary>
+        /// 計算漲跌，方便對照
+        /// </summary>
+        /// <param name="SPs"></param>
+        /// <param name="SPs2"></param>
+        private void CaluAmp(List<SeriesPoint> SPs, List<SeriesPoint> SPs2)
+        {
+            List<SeriesPoint> SPs3 = new List<SeriesPoint>();
+            List<SeriesPoint> SPs4 = new List<SeriesPoint>();
+
+            double s1_startValue = s1_AxisRange.StartValue;
+            double s2_startValue = s2_AxisRange.StartValue;
+
+            foreach (SeriesPoint sp in SPs)
+            {
+                double v = (sp.Values[0] - s1_startValue) / s1_startValue;
+                SPs3.Add(new SeriesPoint(sp.DateTimeArgument, v));
+            }
+
+            foreach (SeriesPoint sp in SPs2)
+            {
+                double v = (sp.Values[0] - s2_startValue) / s2_startValue;
+                SPs4.Add(new SeriesPoint(sp.DateTimeArgument, v));
+            }
+
+            sss.Points.AddRange(SPs3.ToArray());
+            ssss.Points.AddRange(SPs4.ToArray());
+        }
+
+        private List<DateTime> GetDate()
+        {
+            List<DateTime> dateList = new List<DateTime>();
+
+            DateTime StartDate = new DateTime(2016, 10, 12);
+            int i = 0;
+            while (i < 30)
+            {
+                if (!(StartDate.DayOfWeek == DayOfWeek.Saturday ||
+                    StartDate.DayOfWeek == DayOfWeek.Sunday))
+                {
+                    dateList.Add(StartDate);
+                    i++;
+                }
+                StartDate = StartDate.AddDays(1);
+            }
+
+            return dateList;
+        }
+    }
+
+    /// <summary>
+    /// 寫成class，專業度++
+    /// </summary>
+    public class AxisRange
+    {
+        public double StartValue { get; set; }
+        public double HighValue { get; set; }
+        public double LowValue { get; set; }
+
+        /// <summary>
+        ///漲跌幅上升1, 所需要的成交值
+        /// </summary>
+        public double UnitValue
+        {
+            //這個公式得自漲跌幅公式，推導如下
+            //漲跌幅 = (成交價 - 參考價) / 參考價
+            //要求漲跌幅 == 1，因為漲跌幅以百分比表示，所以代0.01
+            //參考價就是StartValue
+            //0.01 = (成交價 - startValue) / startValue
+            //因為成交價一定是基於startValue之上或之下，所以成交價可表示成(startValue ± N)
+            //0.01 = (startValue ± N - startValue) / startValue
+            //0.01 = ±N / startValue
+            //±N = startValue * 0.01
+            get { return StartValue * 0.01; }
+        }
+        public double HighProportion { get { return GetProportion(HighValue); } }
+        public double LowProportion { get { return GetProportion(LowValue); } }
+
+        public AxisRange(double s, double h, double l)
+        {
+            this.StartValue = s;
+            this.HighValue = h;
+            this.LowValue = l;
+        }
+
+        /// <summary>
+        /// 算出從StartValue到最高/最低值 需要幾個單位值
+        /// 如果想提高預留空間，可在回傳值加上一常數
+        /// </summary>
+        /// <param name="d">最高/最低</param>
+        /// <returns></returns>
+        private double GetProportion(double d)
+        {
+            double v = d - StartValue;
+            return Math.Round(v / UnitValue, 0, MidpointRounding.AwayFromZero);
+        }
+
+        /// <summary>
+        /// 根據倍率，算出Axis值
+        /// </summary>
+        /// <param name="p">倍率</param>
+        /// <returns></returns>
+        public double GetAxisValue(double p)
+        {
+            return StartValue + UnitValue * p;
+        }
+    }
+}
